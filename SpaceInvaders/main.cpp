71cac4e60b84999d993685423caee016ac9ebd59
40590148 <40590148@live.napier.ac.uk>
2024-10-01 15:54:33 +0100

    failed attempt

---
+++ SpaceInvaders/main.cpp
@@ -0,0 +1,48 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace sf;
+using namespace std;
+
+const Keyboard::Key controls[4] = {
+	Keyboard::Left,
+	Keyboard::Right,
+};
+
+sf::Texture spritesheet;
+sf::Sprite invader;
+
+const int gameWidth = 800;
+const int gameHeight = 600;
+
+void Load(){
+	if(!spritesheet.loadFromFile("res/img/invaders_sheet.png")){
+		cerr << "Failed to load spritesheet!" << std::endl;
+	}
+	invader.setTexture(spritesheet);
+	invader.setTextureRect(IntRect(Vector2i(0, 0), Vector2i(32, 32)));
+}
+
+void Render(RenderWindow &window){
+	window.draw(invader);
+}
+
+void Update(RenderWindow &window){
+	if(Keyboard::isKeyPressed(Keyboard::Escape)){
+		window.close();
+	}
+}
+
+
+
+int main(){
+  sf::RenderWindow window(sf::VideoMode(gameWidth, gameHeight), "Space Invaders");
+
+  while (window.isOpen()){
+    window.clear();
+	Update(window);
+	Render(window);
+    window.display();
+  }
+  return 0;
+}
