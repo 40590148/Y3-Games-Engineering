4754192e60604090f7c5d52137522facb1bc1dea
40590148 <40590148@live.napier.ac.uk>
2024-10-01 13:56:59 +0100

    Updated main.cpp

--- practical_01/main.cpp
+++ practical_01/main.cpp
@@ -16,6 +16,12 @@ const int gameWidth = 800;
 const int gameHeight = 600;
 const float paddleSpeed = 400.f;
 const float paddleOffsetWall = 10.f;
+const float initialVelocityX = 100.f;
+const float initialVelocityY = 60.f;
+const float velocityMultiplier = 1.1f;
+
+Vector2f ballVelocity;
+bool isPlayer1Serving = false;
 
 CircleShape ball;
 RectangleShape paddles[2];
@@ -31,6 +37,17 @@ void Load(){
 	ball.setRadius(ballRadius);
 	ball.setOrigin(ballRadius / 2, ballRadius / 2);
 	
+	//reset paddle position
+	paddles[0].setPosition(Vector2f(paddleOffsetWall + paddleSize.x / 2.f, gameHeight / 2.f));
+	paddles[1].setPosition(Vector2f(paddleOffsetWall + paddleSize.x / 2.f, gameHeight / 2.f));
+	
+	//reset ball position
+	ball.setPosition(gameWidth / 2, gameHeight / 2);
+	
+	ballVelocity = { (isPlayer1Serving ? initialVelocityX : -initialVelocityX), initialVelocityY };
+}
+
+void Reset(){
 	//reset paddle position
 	paddles[0].setPosition(Vector2f(paddleOffsetWall + paddleSize.x / 2.f, gameHeight / 2.f));
 	paddles[1].setPosition(Vector2f(paddleOffsetWall + paddleSize.x / 2.f, gameHeight / 2.f));
@@ -78,6 +95,32 @@ void Update(RenderWindow &window){
 		direction++;
 	}
 	paddles[1].move(Vector2f(0.f, direction * paddleSpeed * dt));
+	
+	//move the ball
+	ball.move(ballVelocity * dt);
+	
+	//check ball collision
+	const floast bx = ball.getPosition().x;
+	const float by = ball.getPosition().y;
+	
+	if(by > gameHeight){
+		//bottom wall
+		ballVelocity.x *= velocityMultiplier;
+		ballVelocity.y *= -velocityMultiplier;
+		ball.move(Vector2(0.f, -10.f));
+	} else if(by < 0){
+		//top wall
+		ballVelocity.x *= velocityMultiplier;
+		ballVelocity.y *= -velocityMultiplier;
+		ball.move(Vector2(0.f, 10.f));
+	} else if(bx > gameWidth || bx < 0){ //if someone scores, reset
+		Reset();
+/*
+	} else if(bx < paddleSize.x + paddleOffsetWall && by > paddles[0].getPosition().y - (paddleSize.y * 0.5) && by < paddles[0].getPosition.y + (paddleSize.y * 0.5)){ //if the ball collides with the left paddle, bounce the ball
+		
+	} else if(bx < paddleSize.x + paddleOffsetWall && by > paddles[1].getPosition().y - (paddleSize.y * 0.5) && by < paddles[1].getPosition.y + (paddleSize.y * 0.5)){ //if the ball collides with the right paddle, bounce the ball
+*/
+	};
 }
 
 void Render(RenderWindow &window){
